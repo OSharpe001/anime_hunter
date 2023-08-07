db58e4ade2d633af3d6bcaebdbad69e452073689
O. Sharpe <osharpe001@gmail.com>
2023-08-07 13:29:16 -0400

    got API call to stop giving errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import GetAnime from './components/GetAnime';
 
 export default function App() {
   return (
     <div className="App">
       <h1>Anime Hunter</h1>
+      <GetAnime />
     </div>
   );
 };
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -3,23 +3,23 @@ import axios from 'axios';
 const BASE_URL = "https://anime-db.p.rapidapi.com";
 
 const options = {
-  method: 'GET',
+  // method: 'GET',
 //   url: 'https://anime-db.p.rapidapi.com/anime',
-  params: {
-    page: '1',
-    size: '10',
-    // search: 'Fullmetal',
-    // genres: 'Fantasy,Drama',
-    sortBy: 'ranking',
-    sortOrder: 'asc'
-  },
+  // params: {
+  //   page: '1',
+  //   size: '10',
+  //   // search: 'Fullmetal',
+  //   // genres: 'Fantasy,Drama',
+  //   sortBy: 'ranking',
+  //   sortOrder: 'asc'
+  // },
   headers: {
-    'X-RapidAPI-Key': process.env.API_KEY,
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
   }
 };
 
 export const fetchFromAPI = async (url) => {
-    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-    return data;
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
 };
