90daec8b8a38ad1cd9c134e408691cd753bc941a
O. Sharpe <osharpe001@gmail.com>
2023-08-08 00:06:57 -0400

    Seting up Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+// import GetAnime from './components/GetAnime';
+// import DisplayAnimeList from './components/DisplayAnimeList';
+import { Header, Footer, GetAnime, DisplayAnimeList } from "./components"
 import './App.css';
-import GetAnime from './components/GetAnime';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [animeList, setAnimeList] = useState([]);
+
   return (
     <div className="App">
       <h1>Anime Hunter</h1>
-      <GetAnime />
+      <Header />
+      <Routes>
+            {/* <Route path="/" element={<HomePage />}/> */}
+            <Route path="/hunt" element={<GetAnime />}/>
+            <Route path="/bounty" element={<DisplayAnimeList animeList={animeList} />}/>
+            {/* <Route path="/" element={<HomePage />}/> */}
+      </Routes>
+      <Footer />
+      <GetAnime setAnimeList={setAnimeList}/>
     </div>
   );
 };
