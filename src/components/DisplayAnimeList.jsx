88f6a381a0a5099bd2ce30ea8eee9dfaf5b4674e
O. Sharpe <osharpe001@gmail.com>
2023-08-11 03:09:58 -0400

    fixed list redundant keys issue

--- src/components/DisplayAnimeList.jsx
+++ src/components/DisplayAnimeList.jsx
@@ -6,8 +6,8 @@ export default function DisplayAnimeList({ animeList, navigate, title, genres, s
   const data = animeList.data;
 
   const setNewPoster = (({ target }) => {
-    console.log("DISPLAYANIMELIST'S SETNEWPOSTER E.TARGET", target.src);
-    setCurrentBackground(target.src);
+    // console.log("DISPLAYANIMELIST'S SETNEWPOSTER E.TARGET", target);
+    setCurrentBackground({class: "character-show-image", image: target.src, alt: "background fan art"});
   });
 
   useEffect(() => {
@@ -20,8 +20,8 @@ export default function DisplayAnimeList({ animeList, navigate, title, genres, s
         <h1>{title || genres.length > 0 ? "The Bounty" : "Beginner's Bounty"}</h1>
         {data ?
           <ul className="anime-list">
-          {data.map(item => (
-            <li key={item.id} className="bounty">
+          {data.map((item,index) => (
+            <li key={`${item.id}-${index}`} className="bounty">
               <div className="title-thumbnail">
                 <h2 className="title">{item.title}</h2>
                 <img src={item.thumb} alt="thumbnail" />
