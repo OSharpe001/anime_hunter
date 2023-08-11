e2887b02c2cee6b2a9832b784627f6bd5939f33d
O. Sharpe <osharpe001@gmail.com>
2023-08-11 03:11:09 -0400

    added file containing personal ranking. data needs cleaning

---
+++ src/components/sharpRanking.js
@@ -0,0 +1,421 @@
+export const sharpRanking = [
+    
+    {
+        englishName: "",
+        asianName: "Mononogatari",
+        rank: "",
+        notes: "still scoping..."
+    },
+    {
+        englishName: "Zom 100: Bucket List of the Dead",
+        asianName: "Zom 100: Zombie ni Naru made ni Shitai 100 no Koto",
+        rank: "👍🏾",
+        notes: "still scoping..."
+    },
+    {
+        englishName: "Classroom for heroes",
+        asianName: "Eiyuu Kyoushitsu",
+        rank: "1/2👍🏾",
+        notes: "still scoping..."
+    },
+    {
+        englishName: "",
+        asianName: "Jujitsu Kaisen",
+        rank: "👍🏾👍🏾",
+        notes: "still scoping... (2nd season)"
+    },
+    {
+        englishName: "Jobless Reincarnation",
+        asianName: "Mushoku Tensei",
+        rank: "**👍🏾👍🏾",
+        notes: "still scoping... (2nd season)"
+    },
+    {
+        englishName: "Reign of the Seven Spellblades",
+        asianName: "Nanatsu no Maken ga Shihai suru",
+        rank: "",
+        notes: "still scoping..."
+    },
+    {
+        englishName: "Princess Connect! Re:Dive",
+        asianName: "",
+        rank: "👎🏾👎🏾",
+        notes: "utterly corny. made for 5yr olds"
+    },
+    {
+        englishName: "Hell's Paradise",
+        asianName: "Jigokuraku",
+        rank: "👍🏾👍🏾",
+        notes: ""
+    },
+    {
+        englishName: "",
+        asianName: "Ousama Ranking: Yuuki no Takarabako",
+        rank: "1/2👎🏾",
+        notes: ""
+    },
+    {
+        englishName: "KamiKatsu: Working For God In A Godless World",
+        asianName: "Kaminaki Sekai no Kamisama Katsudou",
+        rank: "1/2👎🏾",
+        notes: ""
+    },
+    {
+        englishName: "Dead Mount Death Play",
+        asianName: "",
+        rank: "",
+        notes: ""
+    },
+    {
+        englishName: "Heavenly Delusion",
+        asianName: "Tengoku Daimakyou",
+        rank: "",
+        notes: ""
+    },
+    {
+        englishName: "Dr. Stone",
+        asianName: "",
+        rank: "👍🏾👍🏾",
+        notes: "season3"
+    },
+    {
+        englishName: "The Legendary Hero Is Dead",
+        asianName: "Yusha ga Shinda!",
+        rank: "",
+        notes: "ecchi"
+    },
+    {
+        englishName: "I Got a Cheat Skill in Another World and Became Unrivaled in The Real World, Too",
+        asianName: "Isekai de Cheat Skill wo Te ni Shita Ore wa, Genjitsu Sekai wo mo Musou Suru: Level Up wa Jensei wo Kaeta",
+        rank: "👍🏾",
+        notes: "Final episode was rushed"
+    },
+    {
+        englishName: "Rokudo's Bad Girls",
+        asianName: "Rokudou no Onnatachi",
+        rank: "1/2👎🏾",
+        notes: ""
+    },
+    {
+        englishName: "",
+        asianName: "Mashle",
+        rank: "1 1/2👎🏾",
+        notes: ""
+    },
+    {
+        englishName: "",
+        asianName: "Kono Subarashii Sekai ni Bakuen wo",
+        rank: "",
+        notes: ""
+    },
+    {
+        englishName: "Golden Kamuy",
+        asianName: "",
+        rank: "1/2👎🏾",
+        notes: "getting worse with every new season (season4)"
+    },
+    {
+        englishName: "Summoned to Another World… Again",
+        asianName: "Isekai Shoukan wa Nidome desu",
+        rank: "1/2👎🏾",
+        notes: "bad-quality animations"
+    },
+    {
+        englishName: "Vinland Saga",
+        asianName: "",
+        rank: "👍🏾",
+        notes: "1st half of season 2 is slow paced (season 2)"
+    },
+    {
+        englishName: "The Aristocrat's Otherworldly Adventure: Serving Gods Who Go Too Far",
+        asianName: "Tensei Kizoku no Isekai Boukenroku: Jichou wo Shiranai Kamigamino Shito",
+        rank: "👍🏾",
+        notes: ""
+    },
+    {
+        englishName: "Demon Slayer",
+        asianName: "Kimetsu no Yaiba",
+        rank: "**👍🏾👍🏾",
+        notes: "(season 4)"
+    },
+    {
+        englishName: "My Home Hero",
+        asianName: "",
+        rank: "👍🏾👍🏾",
+        notes: ""
+    },
+    {
+        englishName: "Aiyou's Secret Room",
+        asianName: "Aiyou de Mishi",
+        rank: "",
+        notes: "Only made 2episodes😐"
+    },
+    {
+        englishName: "Ranking Of Kings",
+        asianName: "Ousama Ranking",
+        rank: "👍🏾👍🏾",
+        notes: ""
+    },
+    {
+        englishName: "",
+        asianName: "Kono Subarashii Sekai ni Shukufuku wo",
+        rank: "**👍🏾",
+        notes: ""
+    },
+    {
+        englishName: "",
+        asianName: "",
+        rank: "",
+        notes: ""
+    },
+    {
+        englishName: "",
+        asianName: "",
+        rank: "",
+        notes: ""
+    },
+    {
+        englishName: "",
+        asianName: "",
+        rank: "",
+        notes: ""
+    },
+    -Eiyuuou, Bu wo Kiwameru Tame Tenseisu…(Reborn to Master the Blade…)1/2👎🏾-
+    -Shoujo Shuumatsu Ryokou 1/2👎🏾-
+    -Shuumatsu no Harem-
+    -The Fruit of Evolution: Before I Knew it, my life had it made (Shinka no Mi: Shiranai Uchi Ni Kachigumi Jinsei) 👎🏾👎🏾-
+    -Kaiko sareta Ankoku Heishi (30-dai) no Slow na Second Life 👍🏾-
+    -Inspectre (eh. .1/2👍🏾)•
+    -Blue Lock 👍🏾(first season)-
+    -Watashi, Nouryoki we Heikinchi de tte Itta yo ne! 👎🏾-
+    -Futaku no Guild (Immoral Guild)-
+    -Radiant 👎🏾-
+    -The Hidden Dungeon Only I Can Enter (weird incest theme in first half of series)👎🏾-
+    -Nouman Kanren no Skill bakka Agetetara Nazeka Tsuyoku Natta (I Somehow Got Strong By Raising Skills Related To Farming)👎🏾-
+    -The Eminence in Shadow (Kage no Jitsuryokusha ni Naritakute!)👍🏾-
+    -Mob psycho 100**👍🏾-
+    -Shinobi no Ittoki👎🏾-
+    -My hero academia (6th season)**👍🏾•
+    -Tensei Shitara Ken Deshita👍🏾👍🏾-
+    - Aku no Hana(LAME AS FUCK!!)👎🏾👎🏾-
+    -Kao Ni La Zhanshen Xitong (War God System! I'm Counting On You!)(first season 16ep.)👍🏾👍🏾-
+    -Memories👎🏾-
+    -Kanojo, Okarishimasu (Rent a Girlfriend)👍🏾👍🏾-
+    -Overlord***👍🏾👍🏾(4th season)-
+    -Kinsou no Vermeil(a little echi)1/2👍🏾-
+    -Black Summoner (Kuro no Shoukanshi)*👍🏾👍🏾-
+    -Isekai Uncle(Isekai Ojisan)-
+    World Labyrinth (Isekai Meikyuu de Harem wo)(echi)*👍🏾👍🏾-
+    -My Isekai Life(Tensei Kenja no Isekai Life)1/2👍🏾-
+    -Summer Time Rendering *👍🏾👍🏾-
+    -Made in Abyss👍🏾👍🏾(3rd season unnecessarily cryptic)-
+    -Darker Than Black(2nd season was stupid)-
+    -Bastard (corny. Made for horny teens)👎🏾-
+    -No Game No Life (good starting premise but turned a little lame)👎🏾-
+    -No Game No Life Movie (better than series)-
+    -Da Wang Rao Ming(Spare Me, Great Lord)👍🏾👍🏾👍🏾***
+    -Quanzhi Gaoshou👍🏾-
+    -Quanzhi Fashi👍🏾-
+    -Full Time Magister👍🏾-
+    -Wise Man's Grandchild 👍🏾-
+    -Prison School*👍🏾👍🏾-
+    -Mo dao zu shi
+    -Arifureta 👍🏾-
+    -Sekai saikou no ansatsusha, isekai kizoku ni tensei Suru 👍🏾-
+    -Btoom 1/2👍🏾-
+    -Isekai kizoku ni tensei Suru👍🏾-
+    -Platinum End (same creator as Death Note)-
+    -DearS👎🏾👎🏾-
+    -Isekai Cheat Magician-1/2👍🏾
+    -Tsuki ga Michibiku Isekai(finished 1season)1/2👍🏾
+    -Kaifuku Jutsushino Yarinaoshi👍🏾(ep12)-
+    -Seirei Gensouki 👍🏾
+    -100-man no Inochi no Ue ni Ore wa Tatteiru👍🏾
+    -Castlevania **👍🏾👍🏾-
+    -Tokyo Revengers👍🏾-
+    -Records of Ragnarock👎🏾-
+    -invincible (finished first season)**👍🏾👍🏾-
+    -Kyuukyoku Shinka shita Full Dive RPG ga Genjitsu yori mo Kusoge Dattara ***👍🏾👍🏾(finished first season)
+    -Mushoku Tensei👍🏾👍🏾-
+    -In/Spectre-
+    -So I'm a spider, so what?👍🏾-
+    -Log Horizon(started good, middle story bad, 3rd season good)👍🏾
+    -King's Raid: Ishi wo Tsugumono-tach (started good, ended corny)-
+    -Record of Grancrest War👍🏾-
+    -Avatar: The Legend of Korra👎🏾-
+    -Corpse Party: Tortured Souls-
+    -Noblesse (*watch pov and awakening first)
+    -Dragon's Dogma👎🏾-
+    -Ninja Collection👎🏾(stopped@ep9)-
+    -Record of Lodoss War
+    Record of Lodoss War:CotHKnight
+    -Owari no Seraph: Nagoya. . 👍🏾-
+    -Owari no Seraph: Owaranai. .-p
+    -Shiki-
+    -Gantz-
+    -Gantz: The Movie (live action)👎🏾-
+    -Gantz: The Movie 2 (live action)1/2👍🏾-
+    -Made in Abyss 3: The Movie👍🏾-
+    -Kengan Ashura 👍🏾👍🏾(finished season 2)•
+    -The God of High School1/2👍🏾
+    -Shingeki no Bahamut: Genesis👍🏾-
+    -Shingeki no Bahamut: Virgin Soul1/2👍🏾-
+    -Re-Zero*👍🏾👍🏾•
+    -Bayonetta (movie)👍🏾 (ended slightly corny)-
+    -The Promised Neverland👍🏾👍🏾-
+    -Darwin's Game👍🏾
+    -Tower of Gods👍🏾👍🏾•
+    -Hitori no Shita 1/2👍🏾 (discontinued)-
+    -Gleipnir 1/2👍🏾-
+    -Kill La Kill-
+    -Enen no Shouboutai (Fire Force) 👍🏾👍🏾•
+    -Black Clover (started horrible)👍🏾
+    -Deltora Quest (1/2*pg13)-
+    -Gin no Guardian👎🏾- (LAME)
+    -Asu no Yoichi!-
+    -Puni Puni Poemy-👎🏾👎🏾
+    -Excel Saga👎🏾👎🏾 (couldn't get past ep. 4)
+    -Rewrite- (middle to end confusing)
+    -Ragnarok the Animation-(1 1/2👎🏾LAME)
+    -Yu Yu Hakusho*👍🏾-
+    -Seto No Hanayome-
+    -Mondaiji Tachi ga Isekai Kara Kuru Sou Desu Yo-
+    -The Seven Deadly Sins: Revival of the Commandments👍🏾-
+    -The Saga of Tanya the Evil**👍🏾👍🏾-
+    -The Rising of the Sheild Hero***👍🏾👍🏾-
+    -The Rising of the Sheild Hero 2nd season (rushed!!!)👎🏾-
+    -Baki (LAAAME!!)👎🏾👎🏾-
+    -Sirius the Jaeger*-
+    -Boruto (only good parts includes Naruto and/or Sasuke fights)-
+    -Dororo**👍🏾-
+    -Sword Art Online: Alicization*👍🏾-
+    -One Punch Man (season 2)***👍🏾👍🏾-
+    -Tensei shitara Slime Datta Ken***👍🏾👍🏾-
+    -Rosario + Vampire (LOTS of fan service)-
+    -Zetsuen no Tempest*- 
+    -Kaze no Stigma-
+    -Nichijou (couldn't make it past episode 5)👎🏾-
+    -Another*-
+    -Yuragi-sou no Yuuna-san (LOTS of fan service)-
+    -Karakai Jouzu no Takagi-san*-
+    -Majikoi: Oh! Samurai Girls (nudity/ utterly LAME/racist)👎🏾👎🏾-
+    -Elfen Lied*👍🏾-
+    -Absolute Duo*-
+    -Okamisan and her Seven Companions👎🏾-
+    -Aesthetics of a Rouge Hero**👍🏾-
+    -Punchline**-
+    -Blue Exorcist**-
+    -DanMachi***👍🏾👍🏾-
+    -Netoge no Yome wa Onnanoke ja Nai to Omotta*-
+    -Outbreak Company*-
+    -Shomin Sample*-
+    -Death Parade-
+    -Classroom of the Elite*-
+    -Knights & Magic-
+    -Rokka: Braves of the Six Flowers*-
+    -Taboo Tatoo-(started good, lame ending)-
+    -Devils Line*- (started corny)
+    -Samurai Champloo***-
+    -High school of the dead**-
+    -Full metal alchemist: Brotherhood**-
+    -Attack on titan-
+    -Parasite *-
+    -Akame ga kill!*-
+    -Kenichi: The Mightiest Disciple*-(only the first series)
+    -Ajin**-
+    -Magi -"Sinbad section"
+    -One punch man***👍🏾👍🏾-
+    -Seven deadly sins*(1st season)-
+    -Berserk*-
+    -Stein's Gate*- 
+    -Denpa Onna to Seishun Otoku-
+    -Texhnolyze-
+    -Boondocks*👍🏾-
+    -Isekai Maou to Shoukan Shoujo no Dorei Majutsu*-
+    -Elfen Lied*-
+    -Familiar of Zero-
+    -Owari no seraph*-
+    -Isuca*-
+    -Schwarzes Marken*-
+    -Gakkou Gurashi*-
+    -Brynhildr in the Darkness(uncensored)*-
+    -Another -
+    -Dragonar academy-
+    -Asobi ni Iku yo-
+    -High school dxd**-
+    -Masou Gakuen hxh-
+    -Yosuga no Sora👎🏾-
+    -Mushibugyou-
+    -Baki the Grappler**-
+    -Basilisk*-
+    -Hai to Gensou no Grimgar**👍🏾-
+    -Terra Formars-
+    -Divine Gate-
+    -Dimension W*-
+    -The Legend of Legendary Heroes-
+    -Tales of the Abyss*-
+    -Chrome Shelled Regios-
+    -Kingdom*-
+    -The Twelve Kingdoms*-
+    -Blue Gender-
+    -One Outs*-
+    -D Frag*-
+    -Gangsta**- incompleted
+    -Mushishi- (didn't finish 1st season)
+    -In Another World with My Smartphone👎🏾-
+    -Samurai Flamenco👎🏾-
+    -Tsugumomo*-
+    -Eromanga Sensi-
+    -Erased-
+    -Bible Black (original)[hentai]👎🏾-
+    -Baccano! (dubbed)stopped at episode 16👎🏾-
+    -Seitokai Yakuindomo-
+    -Children of the Whales(-up2ep4)
+    -Juni Taisen: Zodiac war-
+    -Soul Eater👎🏾-
+    -Kiss X Sis👎🏾-
+    -Grisaia No Kaijitsu-
+    -Higurashi-
+    -Aki Sora👎🏾👎🏾 (nasty)-
+    -Miss Kobayashi's Dragon Maid-
+    -Your Name (movie)*-
+    -Shimoneta to lu Gainen ga Sonzai-
+    -Magi: Sinbad no Bouken- (5episodes?)
+    -Joukamachi no dandelion👎🏾
+    -Sora no Otoshimono👎🏾-
+    - Senyuu👎🏾-
+    -Desert Punk👎🏾
+    -Golden Boy👎🏾👎🏾
+    -Sakigake!! Otokojuku👎🏾👎🏾(only got to ep. 13: [corny/racist])
+
+    -Tokyo Goul-
+    -Tokyo Goul: Re-
+    -Atom:The Beginning -
+    -Tenchi Muyo👎🏾👎🏾👎🏾 (couldn't get through 1st episode!)
+    -KADO: The Right Answer -
+    -Full Metal Panic-
+    -Re:Creators1/2👎🏾-
+    -Osake wa Fuufu ni Natte kara-
+    - Omiai Aite wa Oshiego👎🏾-
+    -Punchline (*watch it again*)
+
+    {
+        englishName: "",
+        asianName: "",
+        rank: "",
+        notes: ""
+    },
+    {
+        englishName: "Date Alive",
+        asianName: "",
+        rank: "*",
+        notes: "ended lame"
+    },
+    {
+        englishName: "Devilman Crybaby",
+        asianName: "",
+        rank:"👎🏾👎🏾",
+        notes: "amazing visuals, lamest story ever!"
+    }
+    
+];
