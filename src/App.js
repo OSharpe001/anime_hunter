6b101192afe928683470b59eaf40b9132d2ec6f3
O. Sharpe <osharpe001@gmail.com>
2023-08-10 21:50:58 -0400

    added useState to preserve full image. Added poster page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { Header, Footer, HomePage, GetAnime, DisplayAnimeList, About } from "./components";
+import { Header, Footer, HomePage, GetAnime, DisplayAnimeList, About, PosterPage } from "./components";
 import { images } from "./components/images";
 import { fetchFromAPI } from "./utils/fetchFromAPI";
 import './App.css';
@@ -8,7 +8,12 @@ import './App.css';
 
 export default function App() {
 
-  const [currentBackground, setCurrentBackground] = useState(images[Math.floor(Math.random() * images.length)]);
+  const [title, setTitle] = useState("");
+  const [genres, setGenres] = useState([]);
+  const [preserveImage, setPreserveImage] = useState(false);
+  const [watchList, setWatchList] = useState([]);
+  // const [currentBackground, setCurrentBackground] = useState(images[Math.floor(Math.random() * images.length)]);
+  const [currentBackground, setCurrentBackground] = useState(!preserveImage && images[Math.floor(Math.random() * images.length)]);
 
   const navigate = useNavigate();
 
@@ -23,25 +28,44 @@ export default function App() {
     } catch (error) {
         console.log("An error within the useEffect: ", error);
     }
-}, [setAnimeList]);
+  }, [setAnimeList]);
+
+  const toggleImageLock = () => {
+    setPreserveImage(!preserveImage);
+  };
 
   return (
     <div className={`App ${currentBackground.class}`} style={{backgroundImage: `url("${currentBackground.image}")`}}>
-      <Header navigate={navigate}/>
+      <Header
+          preserveImage={preserveImage}
+          setPreserveImage={setPreserveImage}
+          navigate={navigate}
+          />
       <Routes>
             <Route path="/" element={<HomePage navigate={navigate} />} />
             <Route path="/hunt" element={<GetAnime
+                                            title={title}
+                                            setTitle={setTitle}
+                                            genres={genres}
+                                            setGenres={setGenres}
                                             setAnimeList={setAnimeList}
                                             navigate={navigate}
                                           />} />
             <Route path="/bounty" element={<DisplayAnimeList
                                               animeList={animeList}
+                                              title={title}
+                                              genres={genres}
                                               navigate={navigate}
+                                              setCurrentBackground={setCurrentBackground}
+                                              watchList={watchList}
+                                              setWatchList={setWatchList}
                                             />} />
             <Route path="/guild" element={<About
                                               navigate={navigate}
                                             />} />
+            <Route path="/art" element={<PosterPage />} />
       </Routes>
+      <button onClick={toggleImageLock}>{preserveImage ? "Unlock Image" : "Lock Image"}</button>
       <Footer />
     </div>
   );
