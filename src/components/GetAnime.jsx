b8a898f9de725f3650737a2f4cbf8d1f45c697d2
O. Sharpe <osharpe001@gmail.com>
2023-08-12 16:58:44 -0400

    placed setTimeout on navigate function to wait till the info returns from fetch call. a little clean up

--- src/components/GetAnime.jsx
+++ src/components/GetAnime.jsx
@@ -5,7 +5,6 @@ import Footer from "./Footer";
 export default function GetAnime({ setAnimeList, navigate, title, setTitle, genres, setGenres }) {
 
     const handleChange = ({ target }) => {
-        // console.log("GETANIME'S HANDLECHANGE NAME AND VALUE", target.name, target.value);
         let name = target.name;
         let value = target.value;
         name === "title" && setTitle(value);
@@ -15,8 +14,8 @@ export default function GetAnime({ setAnimeList, navigate, title, setTitle, genr
     const handleSubmit = (e) => {
         e.preventDefault();
         fetchFromAPI(`anime?page=1&size=10${genres.length>0 ? `&genres=${genres.slice(0).join(",")}` : ""}&sortBy=ranking&sortOrder=asc${title ? `&search=${title}` : ""}`)
-            .then((data) => { setAnimeList(data) });
-        navigate("/bounty");
+            .then((data) => { setAnimeList(data) })
+        setTimeout(navigate, 1000, "/bounty");
     };
 
   return (
@@ -26,7 +25,7 @@ export default function GetAnime({ setAnimeList, navigate, title, setTitle, genr
             <form action="">
                 <label htmlFor="title">Title</label>
                 <input onChange={handleChange} type="text" name="title" placeholder="Anime Title" value={title}/>
-                <label htmlFor="genres">{`Genres\n(if more than one, seperate with commas)`}</label>
+                <label htmlFor="genres">Genres<br/>(if more than one, seperate with commas)</label>
                 <input onChange={handleChange} type="text" name="genres" placeholder="Genres " value={genres}/>
                 <button onClick={e=>handleSubmit(e)} type="submit" className="get-list">Check the Bounty</button>
             </form>
