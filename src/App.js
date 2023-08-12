6d0a21bf9169fbf8c18158cb0d84aeef9315bee5
O. Sharpe <osharpe001@gmail.com>
2023-08-12 09:59:08 -0400

    got local storage settings to act right

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { Header, /*Footer,*/ HomePage, GetAnime, DisplayAnimeList, About, PosterPage, WatchList } from "./components";
+import { Header, HomePage, GetAnime, DisplayAnimeList, About, PosterPage, WatchList } from "./components";
 import { images } from "./components/images";
 import { fetchFromAPI } from "./utils/fetchFromAPI";
 import './App.css';
@@ -8,44 +8,57 @@ import './App.css';
 
 export default function App() {
 
-  // const [watchListTitles, setWatchListTitles] = useState("");
   const [title, setTitle] = useState("");
   const [genres, setGenres] = useState([]);
   const [preserveImage, setPreserveImage] = useState(false);
   const [watchList, setWatchList] = useState([]);
 
-  // const [currentBackground, setCurrentBackground] = useState(images[Math.floor(Math.random() * images.length)]);
   const [currentBackground, setCurrentBackground] = useState(!preserveImage && images[Math.floor(Math.random() * images.length)]);
-  // console.log("APP.JS' CURRENTBACKGROUND: ", currentBackground);
+
 
   const navigate = useNavigate();
 
   const [animeList, setAnimeList] = useState([]);
 
-  // ***************************
-  const initializeWatchList = () => localStorage.setItem("WatchList", []);
-
+  // LOCAL STORAGE SETTINGS FOR WATCHLIST (BINGO-BOOK)
   const viewWatchList = () => JSON.parse(localStorage.getItem("WatchList"));
 
   const storeWatchList =  (title) => {
-
-    localStorage.setItem("WatchList", [JSON.stringify(title)]);
+      const oldWatchList = viewWatchList();
+      console.log("OLD WATCHLIST VALUES: ", oldWatchList);
+      if (!viewWatchList()) {
+        console.log("PLACING FIRST ITEM IN WATCHLIST");
+        localStorage.setItem("WatchList", JSON.stringify([title])); 
+        console.log("AFTER PLACING FIRST ITEM IN WATCHLIST- VALUES: ", viewWatchList());
+      } else {
+        if (oldWatchList.includes(title)) {
+          return
+        } else {
+        const newWatchList = [...oldWatchList, title]
+        console.log("NEWWATCHLIST: ", newWatchList);
+        localStorage.setItem("WatchList", JSON.stringify(newWatchList));
+        console.log("WATCHLIST NOW LOOKS LIKE THIS: ", viewWatchList());
+      };
+    };
   };
+
   storeWatchList("Naruto");
   storeWatchList("Bleach");
   storeWatchList("One Peice");
-  viewWatchList();
+  console.log(viewWatchList());
   // ***************************
-  useEffect(() => {
-    try {
-        fetchFromAPI(`anime?page=1&size=10&sortBy=ranking&sortOrder=asc`)
-        .then((data) => {
-            setAnimeList(data);
-        });
-    } catch (error) {
-        console.log("An error within the useEffect: ", error);
-    }
-  }, [setAnimeList]);
+
+  // ***NECESSARY! REMEMBER TO UNCOMMENT WHEN TESTING IS COMPLETE!***
+  // useEffect(() => {
+  //   try {
+  //       fetchFromAPI(`anime?page=1&size=10&sortBy=ranking&sortOrder=asc`)
+  //       .then((data) => {
+  //           setAnimeList(data);
+  //       });
+  //   } catch (error) {
+  //       console.log("An error within the useEffect: ", error);
+  //   }
+  // }, [setAnimeList]);
 
   // const toggleImageLock = () => {
   //   setPreserveImage(!preserveImage);
@@ -53,13 +66,11 @@ export default function App() {
 
   return (
     <div className={`App ${currentBackground.class}`} style={{backgroundImage: `url("${currentBackground.image}")`}}>
-    {/* <div className="App" > */}
       <Header
           preserveImage={preserveImage}
           setPreserveImage={setPreserveImage}
           navigate={navigate}
           />
-      {/* <div className={currentBackground.class} style={{backgroundImage: `url("${currentBackground.image}")`}}> */}
         <Routes>
               <Route path="/" element={<HomePage navigate={navigate} />} />
               <Route path="/hunt" element={<GetAnime
@@ -86,8 +97,6 @@ export default function App() {
               <Route path="/bingo-book" element={<WatchList />} />
         </Routes>
         {/* <button onClick={toggleImageLock}>{preserveImage ? "Unlock Image" : "Lock Image"}</button> */}
-        {/* <Footer /> */}
-      {/* </div> */}
     </div>
   );
 };
