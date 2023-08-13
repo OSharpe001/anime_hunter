f9b26b0918fb7fbf3a34512d0ae30b51c06d4320
O. Sharpe <osharpe001@gmail.com>
2023-08-13 10:54:49 -0400

    got the scratch and erase functions to work and rerender on the watchlist page

--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -1,9 +1,26 @@
 import Footer from "./Footer";
 import close from "../assets/images/close.png";
+import { useEffect, useState } from "react";
+
 
 export default function WatchList({ viewWatchList, toggleWatchListItem, eraseWatchListItem }) {
 
   const watchList = viewWatchList();
+
+  const [scratchTitle, setScratchTitle] = useState("");
+  const [eraseTitle, setEraseTitle] = useState("");
+
+  useEffect(() => {
+    if (scratchTitle) {
+      toggleWatchListItem(scratchTitle);
+      setScratchTitle("");
+    };
+
+    if (eraseTitle) {
+      eraseWatchListItem(eraseTitle);
+      setEraseTitle("");
+    };
+  }, [scratchTitle, eraseTitle]);
 
   return (
     <>
@@ -12,8 +29,8 @@ export default function WatchList({ viewWatchList, toggleWatchListItem, eraseWat
           <div className="bingo-book">
             {watchList.map(title =>
               <div key={title.name}className="bingo-book-entry">
-                <p  onClick={() => toggleWatchListItem(title.name)} className={title.active ? "" : "crossed"}>{title.name}</p>
-                <button className="eraser-button" onClick={() => eraseWatchListItem(title.name)}>
+                <p  onClick={() => setScratchTitle(title.name)} className={title.active ? "" : "crossed"}>{title.name}</p>
+                <button className="eraser-button" onClick={() => setEraseTitle(title.name)}>
                   <img className="eraser-x" src={close} alt="an x" />
                 </button>
               </div>
