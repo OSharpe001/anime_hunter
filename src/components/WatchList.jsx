c7da06f3c1d5b08e18362427aabeebd10c38cae5
O. Sharpe <osharpe001@gmail.com>
2023-08-13 01:43:38 -0400

    added watch list info and buttons to bingo book

--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -1,12 +1,24 @@
 import Footer from "./Footer";
+import close from "../assets/images/close.png";
 
+export default function WatchList({ viewWatchList, toggleWatchListItem, eraseWatchListItem }) {
 
-export default function WatchList() {
+  const watchList = viewWatchList();
 
   return (
     <>
-      <section className="bingo-book">
+      <section className="watch-list">
           <h1>BingoBook</h1>
+          <div className="bingo-book">
+            {watchList.map(title =>
+              <div key={title.name}className="bingo-book-entry">
+                <p  onClick={() => toggleWatchListItem(title.name)} className={title.active ? "" : "crossed"}>{title.name}</p>
+                <button className="eraser-button" onClick={() => eraseWatchListItem(title.name)}>
+                  <img className="eraser-x" src={close} alt="an x" />
+                </button>
+              </div>
+              )}
+          </div>
       </section>
       <Footer />
     </>
