3317e296312cdc46aa3d2689694f814d5f087f08
O. Sharpe <osharpe001@gmail.com>
2023-11-07 23:33:17 -0500

    changed portfolio page link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
     <footer>
       <p>Sharpe Media©    2023</p>
       <nav>
-        <Link className="nav-item button" aria-label="On Click" target="_blank" to="https://osharpesportfolio.vercel.app" >O. Sharpe's Portfolio</Link>
+        <Link className="nav-item button" aria-label="On Click" target="_blank" to="https://osharpesportfolio.netlify.app" >O. Sharpe's Portfolio</Link>
       </nav>
     </footer>
   );
